c0ae7b3f9cb0776a3fca88cafe0e6a7b01c12895
Jeff Grant <jgrant274@gmail.com>
2021-03-15 16:55:39 -0400

    fixed player bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,7 +59,8 @@ var startGame = function(){
     playerInfo.reset();
 for(var i = 0; i < enemyInfo.length; i++){
     if (playerInfo.health > 0){
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1))
+        //debugger;
     var pickedEnemyObj = enemyInfo[i];
      pickedEnemyObj.health = randomNumber(40, 60);
     fight(pickedEnemyObj);
@@ -127,9 +128,17 @@ var randomNumber = function () {
     var value = Math.floor(Math.random() * (60 - 40 + 1)) + 40;
    return value;
     };
-
+//fuction to set name
+var getPlayerName = function () {
+  var name ="";
+  while (name === "" || name === null){
+    name = prompt ("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
     var playerInfo ={
-        name: window.prompt("What is your robot's name?"),
+        name: getPlayerName(),
         health: 100,
         attack:10,
         money:10,
